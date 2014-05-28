8df2b3422a6fadf39bf3550611f5b1be74cc20e2
Stewart Webb <stewart.james.webb@gmail.com>
2014-05-28 19:59:40 +1000

    created Assessment class
    also implemented skeleton for data loading

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
+// Assessment class
+function Assessment(assName, score, total) {
+    this.assName = assName;
+    this.score = score;
+    this.total = total;
+    this.percentage = (this.score/this.total)*100;
+}
+
+Assessment.prototype.toHTML = function() {
+    var html = $("<div><h3>"+this.assName+"</h3><div>");
+    $(html).append('<div class="progress"> \
+        <div class="progress-bar" style="width: '+this.percentage+'%"> \
+            <span class="sr-only">Subject Mark: '+this.score+'/'+this.total+'</span> \
+            <span>'+this.score+'/'+this.total+'</span> \
+        </div> \
+    </div>');
+    return html;
+};
+
 var getNewAssessment = function() {
     var nameInput = $('input#newAssessmentName');
     var newName = nameInput.val();
@@ -11,7 +30,7 @@ var getNewAssessment = function() {
     var newScoreMax = parseFloat(scoreMaxInput.val()); //may need to change this too
     scoreMaxInput.val('');
 
-    console.log("scoreMax type: "+typeof(newScoreMax));
+    // console.log("scoreMax type: "+typeof(newScoreMax));
     var percentage = (newScore/newScoreMax)*100;
     var newAssessment = $('<div class="list-group-item"><h3>' + newName + '</h3></div>');
     //using \ to escape newlines and keep this readable
@@ -24,12 +43,38 @@ var getNewAssessment = function() {
     return newAssessment;
 }
 
+var addAssessment = function() {
+    $("#emptyAlert").remove()
+    $("#assessmentList").append(getNewAssessment().hide().fadeIn(500));
+
+}
+
+var loadNoAssessments = function() {
+    $("#assessmentList").empty().append('<div class="alert alert-warning" id="emptyAlert">No assessments added yet!</div>')
+}
+
 //document ready
 $(function() {
     $("button#addAssessment").click(function(evt) {
-        $("#assessmentList").append(getNewAssessment().hide().fadeIn(500));
+        addAssessment();
     });
-    //$.plot($("#pieChart"),[ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
+    // TODO - this will be data loading when it works
+    var assessmentdata = []; 
+
+    // prepare document
+    if ( typeof assessmentdata == 'undefined' ) {
+        // no data
+        loadNoAssessments();
+    } else if ( assessmentdata.length == 0 ) {
+        // blank list
+        loadNoAssessments();
+    } else if ( assessmentdata.length > 0 ) {
+        //there is data
+    } else {
+        //some error
+    }
+    
+    // dummy data for 
     var data = [{
         label: "Assignment #1",
         data: 15,
