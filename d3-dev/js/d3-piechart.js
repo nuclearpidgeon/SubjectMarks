1f1128576156830e661120e22c34c2fa9ee783f2
Stewart Webb <stewart.james.webb@gmail.com>
2014-11-08 01:16:41 +1100

    holy shit it's dynamic

--- d3-dev/js/d3-piechart.js
+++ d3-dev/js/d3-piechart.js
@@ -7,10 +7,7 @@ function d3test(identifier) {
 		"primary": "#428bca",
 		"warning": "#f0ad4e"
 	} 
-	var myScale = d3.scale.linear().domain([0, 100]).range([0, 2 * Math.PI]);
-	var vis = d3.select("#"+identifier);
-	//var visenter = vis.enter().append(d3.svg.arc*())
-	vis.data([
+	var sampleData = [
 		{
 			"score": 10,
 			"max": 20
@@ -22,8 +19,14 @@ function d3test(identifier) {
 		{
 			"score": 10,
 			"max": 30
+		},
+		{
+			"score": 10,
+			"max": 30
 		}
-	]);
+
+	];
+
 	var innerArc = function(){
 		return d3.svg.arc() 
 			.innerRadius(60) 
@@ -34,46 +37,67 @@ function d3test(identifier) {
 			.innerRadius(110) 
 			.outerRadius(150) 
 	}
-	var ass1arc = innerArc()
-		.startAngle(myScale(0)) 
-		.endAngle(myScale(75));
-	var ass1succ = outerArc()
-		.startAngle(myScale(0)) 
-		.endAngle(myScale(60));
-	var ass1fail = outerArc()
-		.startAngle(myScale(60)) 
-		.endAngle(myScale(75));
-	var ass2arc = innerArc()
-		.startAngle(myScale(75)) 
-		.endAngle(myScale(100));
-	var ass2succ = outerArc()
-		.startAngle(myScale(75)) 
-		.endAngle(myScale(90));
-	var ass2fail = outerArc()
-		.startAngle(myScale(90)) 
-		.endAngle(myScale(100));
-	vis.append("path") 
-		.attr("d", ass1arc) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["warning"]);
-	vis.append("path") 
-		.attr("d", ass1succ) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["success"]);
-	vis.append("path") 
-		.attr("d", ass1fail) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["danger"]);
-	vis.append("path") 
-		.attr("d", ass2arc) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["info"]);
-	vis.append("path") 
-		.attr("d", ass2succ) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["success"]);
-	vis.append("path") 
-		.attr("d", ass2fail) 
-		.attr("transform", "translate(250,200)")
-		.attr("fill", colors["danger"]);
+	var myScale = d3.scale.linear().domain([0, 100]).range([0, 2 * Math.PI]);
+	var pie = d3.layout.pie()
+		.value(function(d) { return d.max }); // set object accessor for pie
+	var vis = d3.select("#"+identifier);
+
+	var arcs = vis.selectAll("g.arc")
+		.data(pie(sampleData))
+		.enter()
+		.append("g")
+		.attr("class", "arc")
+		.attr("transform", "translate(250,200)");
+	arcs.append("path")
+		.attr("fill", function(d, i) {
+		        if (i%2==0) {return colors["warning"]} else {return colors["info"]}
+		    })
+	    .attr("d", innerArc());
+	// vis.data(pie(sampleData))
+	// var visenter = vis.enter()
+	// 	.append("path")
+	// 	.attr("d", innerArc().startAngle())
+	// 	.
+	// var ass1arc = innerArc()
+	// 	.startAngle(myScale(0)) 
+	// 	.endAngle(myScale(75));
+	// var ass1succ = outerArc()
+	// 	.startAngle(myScale(0)) 
+	// 	.endAngle(myScale(60));
+	// var ass1fail = outerArc()
+	// 	.startAngle(myScale(60)) 
+	// 	.endAngle(myScale(75));
+	// var ass2arc = innerArc()
+	// 	.startAngle(myScale(75)) 
+	// 	.endAngle(myScale(100));
+	// var ass2succ = outerArc()
+	// 	.startAngle(myScale(75)) 
+	// 	.endAngle(myScale(90));
+	// var ass2fail = outerArc()
+	// 	.startAngle(myScale(90)) 
+	// 	.endAngle(myScale(100));
+	// vis.append("path") 
+	// 	.attr("d", ass1arc) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["warning"]);
+	// vis.append("path") 
+	// 	.attr("d", ass1succ) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["success"]);
+	// vis.append("path") 
+	// 	.attr("d", ass1fail) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["danger"]);
+	// vis.append("path") 
+	// 	.attr("d", ass2arc) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["info"]);
+	// vis.append("path") 
+	// 	.attr("d", ass2succ) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["success"]);
+	// vis.append("path") 
+	// 	.attr("d", ass2fail) 
+	// 	.attr("transform", "translate(250,200)")
+	// 	.attr("fill", colors["danger"]);
 }
--- d3-dev/js/main.js
+++
@@ -1,7 +0,0 @@
-var myScale = d3.scale.linear().domain([0, 100]).range([0, 2 * Math.PI]);
-var vis = d3.select("#svg_donut");
-var arc = d3.svg.arc() .innerRadius(60) .outerRadius(100) .startAngle(0) .endAngle(1.5*Math.PI);
-var innerArc = d3.svg.arc() .innerRadius(100) .outerRadius(140) .startAngle(1.5*Math.PI).endAngle(2*Math.PI)
-
-vis.append("path") .attr("d", arc) .attr("transform", "translate(300,200)");
-vis.append("path") .attr("d", innerArc) .attr("transform", "translate(300,200)");
