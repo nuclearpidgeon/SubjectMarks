6c23133fd92371c347dd06e07bcd50588fd9a93e
Stewart Webb <stewart.james.webb@gmail.com>
2019-12-27 16:56:53 +1100

    Complete implementation of double pie chart with grouping toggle

--- d3-dev/js/d3-piechart-test.js
+++ d3-dev/js/d3-piechart-test.js
@@ -37,46 +37,153 @@ var viewModel = {
 	sampleData: sampleData,
 	removeDatum: function(el) {
 		sampleData.remove(el);
-	}
+	},
+	groupByEarntLostMode: ko.observable(false)
 }
 
-var getEarntLostArray = function() {
-	var currentData = sampleData();
-	var output = [];
-	for (var i = 0; i < currentData.length; i++) {
-		var subject = currentData[i];
+var makeEarntLostData = function(subjectData) {
+	var markChunks = [];
+	var totalEarnt = 0;
+	var totalLost = 0;
+
+	for (var i = 0; i < subjectData.length; i++) {
+		var subject = subjectData[i];
+
+		totalEarnt += subject.score;
+		totalLost += subject.max - subject.score;
+
 		var percentageEarnt = ((subject.score / subject.max) * subject.weighting);
 		var percentageLost = (((subject.max - subject.score) / subject.max) * subject.weighting);
 
-		output.push({
-			"color": colors["success"],
-			"value": percentageEarnt
+		markChunks.push({
+			"subjectIdx": i,
+			"color": subject.color,
+			"isEarntMarks": true,
+			"value": percentageEarnt,
+			"weighting": subject.weighting
 		});
-		output.push({
-			"color": colors["danger"],
-			"value": percentageLost
+		markChunks.push({
+			"subjectIdx": i,
+			"color": subject.color,
+			"isEarntMarks": false,
+			"value": percentageLost,
+			"weighting": subject.weighting
 		});
 	}
-	return output;
+	return {
+		markChunks: markChunks,
+		totalEarnt: totalEarnt,
+		totalLost: totalLost
+	};
 }
 
 function d3test(identifier) {
 	var weightingArc = function(){
 		return d3.svg.arc() 
-			.innerRadius(70) 
-			.outerRadius(110)
+		.innerRadius(110) 
+		.outerRadius(150)
 	}
 	var scoresArc = function(){
 		return d3.svg.arc() 
-			.innerRadius(110) 
-			.outerRadius(125) 
+		.innerRadius(150) 
+		.outerRadius(165) 
 	}
+	// pseudo-arc for centering labels correctly
+	var labelsArc = function(){
+		return d3.svg.arc() 
+			.innerRadius(110)
+			.outerRadius(165)
+}
 	var myScale = d3.scale.linear().domain([0, 100]).range([0, 2 * Math.PI]);
 
 	var vis = d3.select("#"+identifier);
 	var chart = vis.select(".chart")
+	var currentSubjectData = sampleData();
+	var currentEarntLostData = makeEarntLostData(currentSubjectData);
+	var groupingByEarntLost = viewModel.groupByEarntLostMode()
 
-	var weightingsGroup = chart.select(".weightings");
+	var markChunksContainer = chart.select(".mark-chunks");
+
+	// the mark chunk pie layout lays out two elements/values for each subject
+	// - one for marks earnt and one for marks lost
+
+	var markChunkPie = d3.layout.pie()
+	markChunkPie.value(function(d) {
+		// Use the amount of marks earnt or lost as the value for each wedge
+		// in the pie
+		return d.value
+	});
+	if (groupingByEarntLost) {
+		// sort the data such that all the earnt marks come first
+		markChunkPie.sort(function(a, b) {
+			if (a.isEarntMarks) {
+				if (b.isEarntMarks) {
+					// order by index
+					return a.subjectIdx - b.subjectIdx
+				}
+				else {
+					// a should be first because it is earnt
+					return -1;
+				}
+			}
+			else {
+				if (b.isEarntMarks) {
+					// b should be first because it is earnt
+					return 1;
+				}
+				else {
+					// order by index
+					return a.subjectIdx - b.subjectIdx
+				}
+			}
+		});
+	}
+	else {
+		// don't sort, show in subject order
+		markChunkPie.sort(null)
+	}
+
+
+	var markChunkArcGroups = markChunksContainer.selectAll("g.mark-chunk")
+		.data(markChunkPie(currentEarntLostData.markChunks));
+
+	// define logic for new/'entering' data points
+	// ===========================================
+
+	var newMarkChunkGroups = markChunkArcGroups.enter()
+
+	// set up top-level SVG group element
+	var newMarkChunkGroup = newMarkChunkGroups.append("g")
+		.attr("class", "mark-chunk");
+
+	newMarkChunkGroup.append("path")
+		.attr("class", "weighting-arc");
+	newMarkChunkGroup.append("path")
+		.attr("class", "score-arc");
+
+	// update all arcs
+	markChunkArcGroups
+		.select("path.weighting-arc")
+			.attr("fill", function(d, i) {
+			        return d.data.color;
+			    })
+			.attr("d", weightingArc());
+	markChunkArcGroups
+		.select("path.score-arc")
+			.attr("fill", function(d, i) {
+					return d.data.isEarntMarks ? colors["success"] : colors["danger"];
+				})
+		    .attr("d", scoresArc());
+
+    // delete
+    markChunkArcGroups
+    	.exit()
+		.remove();
+		
+	// full (non-split) weightings
+
+	var weightingsContainer = chart.select(".weightings");
+	weightingsContainer.attr("display", groupingByEarntLost ? 'none' : null)
 
 	var weightingPie = d3.layout.pie()
 	weightingPie.value(function(d) {
@@ -89,81 +196,86 @@ function d3test(identifier) {
 		return d.weighting
 	});
 	weightingPie.sort(null) // don't sort the values so we can reliably get the scores to show up in line
-	var weightingArcs = weightingsGroup.selectAll("g.weighting-arc")
-		.data(weightingPie(sampleData()));
+	var weightingArcGroups = weightingsContainer.selectAll("g.weighting-arc")
+		.data(weightingPie(currentSubjectData));
 
 	// define logic for new/'entering' data points
 	// ===========================================
 
-	var weightingEnterGroup = weightingArcs.enter()
+	var newWeightingGroups = weightingArcGroups.enter()
 
 	// set up top-level SVG group element
-	var newWeightingGroup = weightingEnterGroup.append("g")
+	var newWeightingGroup = newWeightingGroups.append("g")
 		.attr("class", "weighting-arc");
 
 	newWeightingGroup.append("path");
 	newWeightingGroup.append("text");
 
 	// update all arcs
-	weightingArcs
+	weightingArcGroups
 		.select("path")
 			.attr("fill", function(d, i) {
 			        return d.data.color;
 			    })
 		    .attr("d", weightingArc());
-    weightingArcs
+    weightingArcGroups
     	.select("text")
 			.text(function(d, i) { return "#" + (i+1) + " worth " + d.data.weighting + "%"; })
 			.attr("transform", function(d, i) {
-				return "translate(" + weightingArc().centroid(d) + ")";
+				return "translate(" + labelsArc().centroid(d) + ")";
 				})
 			.attr("dy", "0.35em");
 
     // delete
-    weightingArcs
+    weightingArcGroups
     	.exit()
     	.remove();
+
 
 	// scores
 
-	var scoresGroup = chart.select(".scores");
-	var scoresPie = d3.layout.pie();
-	scoresPie.value(function(d) {
+	var scoreTotalsContainer = chart.select(".score-totals");
+	scoreTotalsContainer.attr("display", groupingByEarntLost ? null : 'none')
+
+	var scoreTotalsPie = d3.layout.pie();
+	scoreTotalsPie.value(function(d) {
 		return d.value
 	})
-	scoresPie.sort(null); // don't sort the scores as their order is important
-	var scoresArcs = scoresGroup.selectAll("g.score-arc")
-		.data(scoresPie(getEarntLostArray()));
+	scoreTotalsPie.sort(null); // don't sort
 
-	var scoresEnterGroup = scoresArcs.enter()
+	var scoresArcGroups = scoreTotalsContainer.selectAll("g.score-arc")
+		.data(scoreTotalsPie(
+			[{
+				value: currentEarntLostData.totalEarnt,
+				isEarntMarks: true
+			}, {
+				value: currentEarntLostData.totalLost,
+				isEarntMarks: false
+			}]
+		));
 
-	var newScoreGroup = scoresEnterGroup.append("g")
+	var newScoreGroup = scoresArcGroups.enter()
+
+	var newScoreGroup = newScoreGroup.append("g")
 		.attr("class", "score-arc");
 
 	newScoreGroup.append("path");
-	newScoreGroup.append("text");
 
 	// update all scores
-	scoresArcs
+	scoresArcGroups
 		.select("path")
 			.attr("fill", function(d, i) {
-			        return d.data.color;
+			        return d.data.isEarntMarks ? colors["success"] : colors["danger"];
 			    })
 			// .attr("stroke", "black")
 			// .attr("stroke-width", "1")
-		    .attr("d", scoresArc());
-    scoresArcs
-    	// .select("text")
-		// 	.text(function(d, i) { return i + "(" + d.value + ")"; })
-		// 	.attr("transform", function(d, i) {
-		// 		return "translate(" + scoresArc().centroid(d) + ")";
-		// 		})
-		// 	.attr("dy", "0.35em");
+			.attr("d", scoresArc());
 
     // delete
-    scoresArcs
+    scoresArcGroups
     	.exit()
     	.remove();
+
 }
 
 $('#addData').on('click', function() {
@@ -180,10 +292,13 @@ $('#addData').on('click', function() {
 
 $(function() {
 	ko.applyBindings(viewModel);
-	sampleData.subscribe(function() {
+	var updateGraph = function() {
 		d3test('svg_donut');
-	});
-	d3test('svg_donut');
+	};
+	sampleData.subscribe(updateGraph);
+	viewModel.groupByEarntLostMode.subscribe(updateGraph);
+	
+	updateGraph();
 });
 
 d3.layout.scorePie = function() {
